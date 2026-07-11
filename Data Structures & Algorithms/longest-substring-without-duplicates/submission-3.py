30dfd0c0fd28a27cb3b9d67a4b2f86b9ee1a7be5
Alex Tran <159596960+AlexTran01@users.noreply.github.com>
2026-07-11 15:55:56 -0700

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hashmap = {} # (character)
+        l = 0
+        r = 0
+        max_length = 0
+        
+        while r < len(s):
+            if s[r] not in hashmap:
+                hashmap[s[r]] = r
+            else:
+                max_length = max(max_length, r - l)
+                temp = hashmap.pop(s[r])
+
+                for i in range(l, temp):    
+                    hashmap.pop(s[i])
+                l = temp + 1
+
+                hashmap[s[r]] = r
+
+            r += 1
+        
+        return max(max_length, r-l)
