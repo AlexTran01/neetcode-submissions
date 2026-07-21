921e1075b7ef050e61f16d668b460ba366754b7a
Alex Tran <159596960+AlexTran01@users.noreply.github.com>
2026-07-21 00:18:47 -0700

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode()
+        prev = dummy
+
+        while True:
+
+            minNode = ListNode(float("infinity"))
+            pos_minNode = -1
+
+            for i in range(len(lists)):
+                if lists[i] and lists[i].val < minNode.val:
+                    minNode = lists[i]
+                    pos_minNode = i
+                    
+            if minNode.val == float("infinity"):
+                break
+
+            lists[pos_minNode] = lists[pos_minNode].next
+            
+            prev.next = minNode
+            prev = minNode
+
+        return dummy.next
+    
