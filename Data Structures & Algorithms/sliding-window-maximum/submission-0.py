3d2382cfcfdbee58eb04bda216e8321256b9b40b
Alex Tran <159596960+AlexTran01@users.noreply.github.com>
2026-07-22 18:02:41 -0700

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        q = deque() # pronounce "deck"
+        l = r = 0
+
+        while r < len(nums):
+
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+
+            q.append(r)
+
+            if l > q[0]: # if left pointer has passed the pos of max in deque
+                q.popleft()
+
+            if (r + 1) >= k:
+                res.append(nums[q[0]])
+                l += 1
+
+            r += 1
+        return res
+
